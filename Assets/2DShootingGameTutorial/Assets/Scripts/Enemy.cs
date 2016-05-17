fcdf72daa370657dc50303bb1868cec7676e5274
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-05-17 18:46:11 +0900

    enemy.cs追加
    敵が下に動くところまで作成

---
+++ Assets/2DShootingGameTutorial/Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    Spaceship spaceship;
+
+    void Start()
+    {
+        spaceship = GetComponent<Spaceship>();
+        spaceship.Move(transform.up * -1);
+    }
+
+}
