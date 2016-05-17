3b004da9b604ffcde87d90edaa9efe7d02aee0bb
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-05-17 20:07:15 +0900

    弾のオブジェクトを消す処理追加

--- Assets/2DShootingGameTutorial/Assets/Scripts/Bullet.cs
+++ Assets/2DShootingGameTutorial/Assets/Scripts/Bullet.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class Bullet : MonoBehaviour
 {
     public int speed = 10;
+    public float lifeTime = 5;
 
     void Start()
     {
         GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+        Destroy(gameObject, lifeTime);
     }
 }
