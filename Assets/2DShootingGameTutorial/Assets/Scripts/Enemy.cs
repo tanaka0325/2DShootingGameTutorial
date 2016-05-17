c4428cbc813bc43f2d59eb3227f47fa36a8ed4b4
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-05-17 18:57:41 +0900

    enemyに弾を打つ処理追加

--- Assets/2DShootingGameTutorial/Assets/Scripts/Enemy.cs
+++ Assets/2DShootingGameTutorial/Assets/Scripts/Enemy.cs
@@ -5,10 +5,20 @@ public class Enemy : MonoBehaviour
 {
     Spaceship spaceship;
 
-    void Start()
+    IEnumerator Start()
     {
         spaceship = GetComponent<Spaceship>();
         spaceship.Move(transform.up * -1);
+
+        while (true)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Transform shotPosition = transform.GetChild(i);
+                spaceship.Shot(shotPosition);
+            }
+            yield return new WaitForSeconds(spaceship.shotDelay);
+        }
     }
 
 }
