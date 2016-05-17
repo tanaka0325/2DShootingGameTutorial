4830905906bb4c8a16349926f75bfb9162ab8903
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-05-17 19:57:59 +0900

    当たり判定修正
    すべてに当たり判定があるわけではないので、layer collision matrixで制御するように

--- Assets/2DShootingGameTutorial/Assets/Scripts/Player.cs
+++ Assets/2DShootingGameTutorial/Assets/Scripts/Player.cs
@@ -26,8 +26,17 @@ public class Player : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D c)
     {
-        Destroy(c.gameObject);
-        spaceship.Explosion();
-        Destroy(gameObject);
+        string layerName = LayerMask.LayerToName(c.gameObject.layer);
+
+        if (layerName == "Bullet (Enemy)")
+        {
+            Destroy(c.gameObject);
+        }
+
+        if (layerName == "Bullet (Enemy)" || layerName == "Enemy")
+        {
+            spaceship.Explosion();
+            Destroy(gameObject);
+        }
     }
 }
