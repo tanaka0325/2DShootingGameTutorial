9afcd4d2953632b64478786c6e1e496120bf020d
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-05-18 12:07:34 +0900

    scoreのUI追加

---
+++ Assets/2DShootingGameTutorial/Assets/Scripts/Score.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public GUIText scoreGUIText;
+    public GUIText highScoreGUIText;
+
+    private int score;
+    private int highScore;
+    private string highScoreKey = "highScore";
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    void Update()
+    {
+        if (highScore < score)
+        {
+            highScore = score;
+        }
+
+        scoreGUIText.text = score.ToString();
+        highScoreGUIText.text = "HighScore : " + highScore.ToString();
+    }
+
+    private void Initialize()
+    {
+        score = 0;
+        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt(highScoreKey, highScore);
+        PlayerPrefs.Save();
+
+        Initialize();
+    }
+}
